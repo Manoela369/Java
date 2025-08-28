646a6279b225e0c8b0e10a09488f54482f7304dd
Manoela369 <manoela.geres@escola.pr.gov.br>
2025-08-28 12:19:10 -0300

    Parte do código

---
+++ exercício.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class Cadastro{
+    public static void main(String[] args){
+        String nome= JOptionPane.showInputDialog("Qual seu nome?");
+//       String sobreNome= JOptionPane.showInputDialog("Qual seu sobrenome?");
+        public class Cadastro{
+            
+        }
+    }
+}
